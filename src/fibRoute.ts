766b341fd169b82e9f96bdbe13e7e861de278d91
Daryl <daryldocteur13@gmail.com>
2025-08-26 22:08:33 -0400

    Changed fibRoute to improve type safety

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,15 +1,20 @@
 // Endpoint for querying the fibonacci numbers
-
+import { Request, Response } from "express";
 import fibonacci from "./fib";
 
-export default (req, res) => {
+export default (req: Request, res: Response) => {
   const { num } = req.params;
 
-  const fibN = fibonacci(parseInt(num));
-  let result = `fibonacci(${num}) is ${fibN}`;
+  const n = parseInt(num, 10);
+  if (isNaN(n)) {
+    return res.send(`Invalid input: ${num}`);
+  }
+
+  const fibN = fibonacci(n);
+  let result = `fibonacci(${n}) is ${fibN}`;
 
   if (fibN < 0) {
-    result = `fibonacci(${num}) is undefined`;
+    result = `fibonacci(${n}) is undefined`;
   }
 
   res.send(result);
